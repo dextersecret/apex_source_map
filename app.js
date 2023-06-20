bbe5c7d20cbbaf0470d7fe2b99c24b3c43371d48
Vladimir Mironov <vladimir.mironov@desynit.com>
2023-06-20 12:13:42 +0100

    viewer settings and optional tests

--- app.js
+++ app.js
@@ -1,20 +1,23 @@
 const fs = require('fs');
 const path = require('path');
+const classNames = [];
+const includeTests = process.argv.includes('t');
 
 // Define the directory containing your Apex classes
 const apexClassesDirectory = 'C:/Users/vlmi/Documents/myVSCodeProjects/Feb23/bts-sfdx/force-app/main/default/classes';
-
 // Define the directory where Obsidian notes will be created
 const obsidianNotesDirectory = './BTS_apex_map_Obsidian_files';
 
-// Recursive function to explore directories and create notes
-function processDirectory() {
-    makeObsidianNotes();
+function run() {
+    classNames.push(...getFileNames(apexClassesDirectory));
+    deleteOldNotes(obsidianNotesDirectory);
+    processDirectory(apexClassesDirectory);
 }
 
-function makeObsidianNotes() {
-    fs.readdirSync(apexClassesDirectory, { withFileTypes: true }).forEach((dirent) => {
-        const fullPath = path.join(apexClassesDirectory, dirent.name);
+// Recursive function to explore directories and create notes
+function processDirectory(directory) {
+    fs.readdirSync(directory, { withFileTypes: true }).forEach((dirent) => {
+        const fullPath = path.join(directory, dirent.name);
 
         if (dirent.isDirectory()) {
             // If the current item is a directory, explore it
@@ -28,8 +31,6 @@ function makeObsidianNotes() {
 
 // Function to create an Obsidian note from a .cls file
 function createObsidianNote(filePath) {
-    const includeTests = false;
-    let isTest = false;
     let fileContent = fs.readFileSync(filePath, 'utf8');
 
     // Create a note title from the file name (without the extension)
@@ -38,39 +39,86 @@ function createObsidianNote(filePath) {
     // Create the note file in the Obsidian notes directory
     const notePath = path.join(obsidianNotesDirectory, `${noteTitle}.md`);
 
-    // Check if the file content starts with "@isTest" and add a tag if so
     let noteContent = '';
+    // Check if the file content starts with "@isTest" and add a tag if so
     if (fileContent.trimStart().toLowerCase().startsWith('@istest') || noteTitle.toLowerCase().endsWith('test')) {
-        noteContent += '#isTest\n\n';
-        isTest = true;
-        if (isTest && !includeTests) {
+        // Check if the "t" parameter was provided, exclude test classes if not
+        if ( includeTests ) {
+            noteContent += '#isTest\n\n';
+        } else {
             return;
         }
     }
 
-    if (!isTest) {
-        // Add the file content to the note as a fenced code block
-        noteContent += '```java\n' + fileContent + '\n```';
-        // Write the note content to the note
-        fs.writeFileSync(notePath, noteContent);
-    }
+    noteContent += insertLinksToOtherClasses(fileContent);
+    // Add the file content to the note as a fenced code block
+    noteContent += '```java\n' + fileContent + '\n```';
+    // Write the note content to the note
+    fs.writeFileSync(notePath, noteContent);
 
 }
 
-// List internal classes
-function listInternalClasses(fileContent){
-    let internalClassNames = [];
+// Function to read all class names in the source directory and its subdirectories
+function getFileNames(sourceDirectory) {
+    let classNames = [];
 
-    const newInstanceRegExp = /\b(Class)\s+([\w]+)(\s+)?\(.*?\)/gi;
-    let match;
+    // Use a helper function to recursively search subdirectories
+    function searchDirectory(directory) {
+        const files = fs.readdirSync(directory);
 
-    while ((match = newInstanceRegExp.exec(fileContent)) !== null) {
-        if ( match[1] != null ) {
-            internalClassNames.add(match[1]);
+        for (const file of files) {
+            const filePath = path.join(directory, file);
+            const stat = fs.statSync(filePath);
+
+            if (stat.isDirectory()) {
+                // If the file is a directory, search its contents
+                searchDirectory(filePath);
+            } else if (path.extname(file) === '.cls') {
+                // If the file is a .cls file, add its name (without the extension) to the array
+                classNames.push(path.basename(file, '.cls'));
+            }
         }
     }
-    return internalClassNames;
+    searchDirectory(sourceDirectory);
+    return classNames;
+}
+
+function insertLinksToOtherClasses(fileContent){
+    // Create a RegExp to match "new ClassName(), classname.methodName(), etc."
+    const newInstanceRegExp = /\b([\w]+)(\.\w+)?(\.\w+)?(\s+)?\(.*?\)/g;
+
+    let match;
+    let linkedClasses = new Set(); // To avoid adding multiple links to the same class
+    noteLinksSection = 'Refers To:\n';
+
+    while ((match = newInstanceRegExp.exec(fileContent)) !== null) {
+        thisMatch = match[1];
+        if ( classNames.includes(thisMatch) &&
+             !thisMatch.toLowerCase().endsWith('__c') &&
+             !linkedClasses.has(thisMatch) ) 
+            {
+                // Add a link to the referenced class in Obsidian format
+                noteLinksSection += `[[${thisMatch}]]\n`;
+                linkedClasses.add(thisMatch);
+            }
+    }
+    return noteLinksSection + '\n\n';
+}
+
+// Function to delete all files in the directory
+function deleteOldNotes(directoryPath) {
+    const files = fs.readdirSync(directoryPath);
+
+    for (const file of files) {
+        const filePath = path.join(directoryPath, file);
+        const stat = fs.statSync(filePath);
+
+        // If the file is not a directory, delete it
+        if (!stat.isDirectory()) {
+            fs.unlinkSync(filePath);
+        }
+    }
 }
 
 // Start the exploration
-processDirectory();
+run();
--- readme.txt
+++ readme.txt
@@ -2,15 +2,16 @@ This script:
 
 Reads the given directory and its subdirectories for '.cls' files.
 For each '.cls' file it finds, it creates an Obsidian note with the same name and content in the given Obsidian notes directory.
+It finds relationships between classes and adds them to the notes as links.
 
-
-=== HOW TO USE ===
+=== USING THE APP ===
 0. Install Obsidian: Obsidian is a free, local-first, markdown-based knowledge management tool. Download and install it from the official website: https://obsidian.md/.
 1. Replace 'C:/ ... /force-app/main/default/classes' with your own path before running the script.
-2. run command: `node app.js`
+2. run command: `node app.js` To include tests include the `t` parameter: `node app.js t`
+3. Open Obsidian and point your vault to the directory with notes. Default: './BTS_apex_map_Obsidian_files'.
 
-x. Open Obsidian and point your vault to the directory. Default: './BTS_apex_map_Obsidian_files'.
-y. Use the Graph view to explore the repository.
-z. Use the search bar to search for keywords in the repository.
-%. Use the backlinks to see which classes are using the class you are currently viewing.
-$: Use the outline view to see the class structure.
+===== OBSIDIAN NAVIGATION =====
+- Use the Graph view to explore the repository.
+- Use the search bar to search for keywords in the repository.
+- The links show relationships between classes.
+- Hold Ctrl and hover over a node to see class contents. Click on the node while holding Ctrl to open the file in a new tab.
